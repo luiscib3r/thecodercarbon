e997973ef90f64c471efc3a1f3469360cf61d504
Luis Antonio Correa Leyva <correaleyval@gmail.com>
2021-05-09 00:46:01 -0400

    Init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn main:app -w 4 -t 200 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:$PORT
---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import utility
+import carbon
+from datetime import datetime
+from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get(
+    request: Request
+):
+    data = request.query_params
+    try:
+        code = data['code']
+    except KeyError:
+        return {"error": "Code is required to create a Carbon!"}
+
+    validatedBody = utility.validateBody(data)
+    carbonURL = utility.createURLString(validatedBody)
+    path = f'{os.getcwd()}/{datetime.now().microsecond}.png'
+    await carbon.get_response(carbonURL, path)
+    return FileResponse(path)
+
+
+@app.post("/")
+async def post(
+    request: Request
+):
+    data = await request.json()
+    try:
+        code = data['code']
+    except KeyError:
+        return {"error": "Code is required to create a Carbon!"}
+
+    validatedBody = utility.validateBody(data)
+    carbonURL = utility.createURLString(validatedBody)
+    path = f'{os.getcwd()}/{datetime.now().microsecond}.png'
+    await carbon.get_response(carbonURL, path)
+    return FileResponse(path)
